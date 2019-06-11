7a2caf24ed4fbfc534814b262844af4a6c824151
crackedpixel <acarbajal721@live.com>
2019-06-11 18:58:34 -0500

    Small change to closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,10 +1,27 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+console.log("##############################")
+console.log("########   Task 1    #########")
+console.log("##############################")
+const otherFactory = () => {
+  let numWorkers = 0;
+  return () => {
+    numWorkers++;
+    console.log(numWorkers);
+    return numWorkers;
+  }
+}
 
+const newFactory = otherFactory();
+newFactory();
+newFactory();
+newFactory();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
+console.log("##############################")
+console.log("########   Task 2    #########")
+console.log("##############################")
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
@@ -23,6 +40,10 @@ newCounter();
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+console.log("##############################")
+console.log("########   Task 3    #########")
+console.log("##############################")
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
